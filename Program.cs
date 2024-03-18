a24a769e465e75fc62f50dde81f6290bdbe8ee54
Jēkabs Avotiņš <jekabs.avotins@edu.rtu.lv>
2024-03-18 02:58:46 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 11001);
+            UdpClient udpClient = new UdpClient(endPoint);
+
+            Console.WriteLine("Receiver is running. Waiting for a message.");
+
+            
+            while (true)
+            {
+                byte[] receivedBytes = udpClient.Receive(ref endPoint);
+                string receivedText = Encoding.ASCII.GetString(receivedBytes);
+                Console.WriteLine($"Received message: {receivedText}");
+
+                if (receivedText.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Console.WriteLine("Receiver closed.");
+                    break;
+                }
+
+            }
+            
+            
+            
+        }
+    }
+}
+
+
+
+
